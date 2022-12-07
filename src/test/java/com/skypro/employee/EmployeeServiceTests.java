e2f629a9e29134e890885fc0b1f2355365f2cc38
maks <mamonov.maksim.20003@gmail.com>
2022-12-07 12:37:30 +0500

    работа над тестами 0.6

--- src/test/java/com/skypro/employee/EmployeeServiceTests.java
+++ src/test/java/com/skypro/employee/EmployeeServiceTests.java
@@ -17,21 +17,56 @@ import java.util.stream.Stream;
 @SpringBootTest
 public class EmployeeServiceTests {
 
-    public final EmployeeService employeeService = new EmployeeService();
+    public EmployeeService employeeService;
     Employee employee1 = new Employee("name1", "lastName1", 1, 124354);
     Employee employee2 = new Employee("name2", "lastName2", 2, 123615);
-
+    int maxSalary = 0;
+    int minSalary = 0;
+    int highSalary = 0;
     @BeforeEach
     public void addEmployee() {
-
+        this.employeeService = new EmployeeService();
         employeeService.addEmployee(employee1);
         employeeService.addEmployee(employee2);
+        maxSalary = employeeService.getSalaryMax();
+        minSalary = employeeService.getSalaryMin();
+        highSalary = employeeService.getHighSalary();
+    }
 
+    @Test
+    public void addingTheRightEmployee() {
+        Employee expected = new Employee("firstName3", "lastName3", 3, 1241);
+        Employee actual = this.employeeService.addEmployee(expected);
+    }
+
+    @Test
+    public void getAListOfAllTheWorkers() {
+        EmployeeService current = new EmployeeService();
+        current.addEmployee(employee1);
+        current.addEmployee(employee2);
+        Assertions.assertEquals(employeeService, current);
+    }
+
+    @Test
+    public void getEmployeesWithTheMaxSalary() {
+        Assertions.assertEquals(employeeService.getSalaryMax(), maxSalary);
+    }
+
+    @Test
+    public void getEmployeesWithTheMinSalary() {
+        Assertions.assertEquals(employeeService.getSalaryMin(), minSalary);
+    }
+
+    @Test
+    public void getEmployeesWithTheHighSalary() {
+        Assertions.assertEquals(employeeService.getHighSalary(), highSalary);
     }
 
     @Test
     public void addEmployeeNullException() {
-        Assertions.assertThrows(IllegalArgumentException.class, this::addEmployee);
+        Employee employee3 = new Employee("name3", "lastName3", 3, 2431);
+        employeeService.addEmployee(employee3);
+        Assertions.assertThrows();
     }
 
 
@@ -43,6 +78,7 @@ public class EmployeeServiceTests {
 
     @Test
     public void getAllEmployeesNotNull() {
+
         Assertions.assertNotNull(employeeService);
     }
 
